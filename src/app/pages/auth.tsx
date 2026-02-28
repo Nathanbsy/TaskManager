047e930e02e24b71b9ca4cd7630bf251be4c5081
Nathanbsy <nathanbs1227@gmail.com>
2026-02-28 18:41:06 -0300

    Adicionando o backend - parte 1

--- src/app/pages/auth.tsx
+++ src/app/pages/auth.tsx
@@ -2,7 +2,7 @@ export default function Auth(tipo: string) {
 
     return(
         <div className="">
-            {tipo == "cadastro"}
+            <h1 className="text-3xl font-bold mb-4">{tipo}</h1>
         </div>
     );
 }
